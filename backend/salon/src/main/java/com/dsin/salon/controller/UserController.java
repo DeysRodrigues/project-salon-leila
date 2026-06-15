5b362135f0bccf316b9b83c5c004ba90e20e21ce
Deys <deysprogrammer@gmail.com>
2026-06-15 13:15:55 -0300

    feat: added user controller for profile management

---
+++ backend/salon/src/main/java/com/dsin/salon/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.dsin.salon.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dsin.salon.dto.request.UserRequestDTO;
+import com.dsin.salon.model.User;
+import com.dsin.salon.repository.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> register(@Valid @RequestBody UserRequestDTO request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole("CLIENT");
+
+        return ResponseEntity.status(201).body(userRepository.save(user));
+    }
+}
